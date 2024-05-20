b87837046956e5d059fa2282a2d2998f9638ee2f
AkiChase <1003019131@qq.com>
2024-05-20 22:35:07 +0800

    Update build-server.sh

--- build-server.sh
+++ build-server.sh
@@ -2,6 +2,6 @@ export JAVA_HOME="$(/usr/libexec/java_home --version 1.17)"
 export PATH="$JAVA_HOME/bin:$PATH"
 
 ./gradlew assembleRelease
-cp ./server/build/outputs/apk/release/server-release-unsigned.apk ./scrcpy-server
+cp ./server/build/outputs/apk/release/server-release-unsigned.apk ./scrcpy-mask-server-v2.4
 
-echo "Output: ./scrcpy-server"
+echo "Output: ./scrcpy-mask-server-v2.4"
